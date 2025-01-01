1b97510bc62910aa01b65693ef99f3c50f8e580a
ilgo123 <prayogoilham27@gmail.com>
2025-01-01 12:26:58 +0700

    modify configuration for gsheets

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config:
     SHEET_NAME = 'DBKelanaPustaka'
     BOOKSHEET_NAME = 'Book'
     USERSHEET_NAME = 'User'
-    CREDENTIALS_FILE = 'kelana-pustaka-124a8ed29086.json'
     SCOPES = [
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive'
--- services/google_sheets_service.py
+++ services/google_sheets_service.py
@@ -4,17 +4,30 @@ import streamlit as st
 from config import Config
 
 class GoogleSheetsService:
+    credentials = {
+            "type": st.secrets["type"],
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"],
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": st.secrets["auth_uri"],
+            "token_uri": st.secrets["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+        }
+
     def __init__(self):
         self.book = self._authenticate_book()
         self.user = self._authenticate_user()
 
     def _authenticate_book(self):
         try:
-            creds = ServiceAccountCredentials.from_json_keyfile_name(
-                Config.CREDENTIALS_FILE, 
-                Config.SCOPES
-            )
-            client = gspread.authorize(creds)
+            # creds = ServiceAccountCredentials.from_json_keyfile_name(
+            #     Config.CREDENTIALS_FILE, 
+            #     Config.SCOPES
+            # )
+            client = gspread.service_account_from_dict(self.credentials)
             return client.open(Config.SHEET_NAME).worksheet(Config.BOOKSHEET_NAME)
         except Exception as e:
             st.error(f"Failed to connect to Google Sheets: {e}")
@@ -22,11 +35,11 @@ class GoogleSheetsService:
         
     def _authenticate_user(self):
         try:
-            creds = ServiceAccountCredentials.from_json_keyfile_name(
-                Config.CREDENTIALS_FILE, 
-                Config.SCOPES
-            )
-            client = gspread.authorize(creds)
+            # creds = ServiceAccountCredentials.from_json_keyfile_name(
+            #     Config.CREDENTIALS_FILE, 
+            #     Config.SCOPES
+            # )
+            client = gspread.service_account_from_dict(self.credentials)
             return client.open(Config.SHEET_NAME).worksheet(Config.USERSHEET_NAME)
         except Exception as e:
             st.error(f"Failed to connect to Google Sheets: {e}")
